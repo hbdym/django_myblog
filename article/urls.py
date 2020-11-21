d4e6735d165174f3e0f2b7e70f7befc492e2d9b5
hbdym <hbdym@live.com>
2020-11-21 23:36:32 +0800

    06 - View 及 Template

--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+# 引入views.py
+from . import views
 
 app_name='article'
 
 urlpatterns = [
-    #TODO
+    # path函数将url映射到视图
+    path('article-list/', views.article_list, name='article_list'),
 ]
