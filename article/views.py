546b648cddd100d4c117f223947fd8f9486ca6c2
hbdym <hbdym@live.com>
2020-11-22 22:12:08 +0800

    08.编写文章详情页面

--- article/views.py
+++ article/views.py
@@ -16,3 +16,10 @@ def article_list(request):
     return render(request, 'article/list.html', context)
 
 
+def article_detail(request, id):
+    # 取出相应的文章
+    article = ArticlePost.objects.get(id=id)
+    # 需要传递给模板的对象
+    context = {'article': article }
+    # 载入模板，并返回context对象
+    return render(request, 'article/detail.html', context)
